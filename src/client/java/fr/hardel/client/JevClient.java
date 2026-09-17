ff82b7ce8d3291af66103ce2863b651906990a2d
Hardel <42413417+Hardel-DW@users.noreply.github.com>
2026-09-17 13:47:56 +0200

    feat: initial commit

---
+++ src/client/java/fr/hardel/client/JevClient.java
@@ -0,0 +1,10 @@
+package fr.hardel.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class JevClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
